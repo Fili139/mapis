71b65210d44f335a7adf4b748aa3ec05440ed9a0
Fili139 <filippo139@gmail.com>
2025-01-19 15:29:06 +0100

    beta 0.5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react'
-import { GoogleLogin } from '@react-oauth/google';
-import toast, { Toaster } from 'react-hot-toast';
+import ReactDOMServer from 'react-dom/server';
+import { GoogleLogin} from '@react-oauth/google'
+import toast, { Toaster } from 'react-hot-toast'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import L from 'leaflet';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDroplet } from '@fortawesome/free-solid-svg-icons';
+import { faPizzaSlice } from '@fortawesome/free-solid-svg-icons';
+import 'leaflet/dist/leaflet.css'
 
 import './App.css'
-// import 'leaflet/dist/leaflet.css'
 
 
 function App() {
@@ -14,6 +20,7 @@ function App() {
   const [map, setMap]: any = useState(null)
   const [userLocation, setUserLocation]: any = useState(null)
   const [markers, setMarkers]: any = useState([])
+  const [loadingToastId, setLoadingToastId]: any = useState("")
 
   const initialPosition: any  = { lat: 0, lng: 0 }
 
@@ -73,6 +80,10 @@ function App() {
         // console.log('db response:', _data, 'fetching markers...');
 
         await getUserMarkers(email)
+
+        toast.success("Logged in!", {
+          id: loadingToastId,
+        });
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -108,6 +119,22 @@ function App() {
   }
   */
 
+  const pissIcon = L.divIcon({
+    html: ReactDOMServer.renderToString(
+      <FontAwesomeIcon icon={faDroplet} style={{ color: 'yellow', fontSize: '24px' }} />
+    ),
+    iconSize: [24, 24], // Adjust size if needed
+    className: '' // Optional class for further styling
+  });
+
+  const foodIcon = L.divIcon({
+    html: ReactDOMServer.renderToString(
+      <FontAwesomeIcon icon={faPizzaSlice} style={{ color: 'orange', fontSize: '24px' }} />
+    ),
+    iconSize: [24, 24], // Adjust size if needed
+    className: '' // Optional class for further styling
+  });
+
   const getUserMarkers = async (email: string) => {
     fetch(server+"/api/db/get_user_markers", {
       method: 'POST',
@@ -118,19 +145,18 @@ function App() {
     })
     .then((res) => res.json())
     .then((data) => {
-      // console.log('Backend response:', data);
+      // console.log('Backend response:', data)
 
       const markersList = []
       for (let marker of data.response)
         markersList.push({ type: marker.type, description: marker.description, position: { lat: marker.lat, lng: marker.lng }, date: marker.ts_creation })
 
       setMarkers(markersList)
-
       setShowMap(true)
     })
     .catch((error) => {
-      console.error('Error:', error);
-    });
+      console.error('Error:', error)
+    })
   }
 
   const addMarker = async (type: string) => {
@@ -145,21 +171,24 @@ function App() {
     })
     .then((res) => res.json())
     .then((_data) => {
-      // console.log('Backend response:', _data);
+      // console.log('Backend response:', _data)
 
       setMarkers([...markers, { type: type, description: description, position: { lat: userLocation.lat, lng: userLocation.lng }, date: new Date().toLocaleString() }])
 
-      toast.success("Marker added!");
+      toast.success("Marker added!")
     })
     .catch((error) => {
-      console.error('Error:', error);
-
-      toast.error("Error!");
-    });
+      console.error('Error:', error)
+      toast.error("Error!")
+    })
   }
 
   const handleLogin = (response: any) => {
     // console.log('Google login response:', response);
+
+    const loadingToast = toast.loading("Loading...")
+
+    setLoadingToastId(loadingToast)
 
     // Send the token to your Node.js backend for verification
     fetch(server+"/api/auth/login", {
@@ -177,9 +206,12 @@ function App() {
       setEmail(data.email)
     })
     .catch((error) => {
-      console.error('Error:', error);
-    });
-  };
+      console.error("Error:", error)
+      toast.error("An error occured, try again later", {
+        id: loadingToast,
+      })
+    })
+  }
 
   const CurrentPosition = () => {
     const handleClick = () => {
@@ -194,11 +226,10 @@ function App() {
           top: 10,
           left: 55,
           zIndex: 1000,
-          backgroundColor: "white",
           padding: "10px",
           border: "1px solid gray",
           borderRadius: "5px",
-          cursor: "pointer"
+          cursor: "pointer",
         }}
       >
         Move to current location
@@ -233,23 +264,42 @@ function App() {
   }
   */
 
-  const AddMarker = () => {
+  const AddPissMarker = () => {
     return (
       <button
         onClick={() => addMarker("piss")}
         style={{
           position: "absolute",
           top: 10,
-          left: 230,
+          left: 225,
           zIndex: 1000,
-          backgroundColor: "white",
           padding: "10px",
           border: "1px solid gray",
           borderRadius: "5px",
           cursor: "pointer"
         }}
       >
-        Add marker
+        Piss
+      </button>
+    )
+  }
+
+  const AddFoodMarker = () => {
+    return (
+      <button
+        onClick={() => addMarker("food")}
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 280,
+          zIndex: 1000,
+          padding: "10px",
+          border: "1px solid gray",
+          borderRadius: "5px",
+          cursor: "pointer"
+        }}
+      >
+        Food
       </button>
     )
   }
@@ -262,6 +312,23 @@ function App() {
     )
   }
 
+  const getGoogleMapsDirections = (lat: number, lng: number) => {
+    // const url = `https://www.google.com/maps?q=${lat},${lng}`;
+    const url = `https://www.google.com/maps/dir/?api=1&origin=${userLocation.lat},${userLocation.lng}&destination=${lat},${lng}&travelmode=""`;
+    window.open(url, '_blank'); // Open in a new tab
+  };
+
+  /*
+  function ButtonControl({text}: any) {
+    return (
+        <div className={'leaflet-top leaflet-right'}>
+          <div className="leaflet-control">
+            <button type={"button"} onClick={() => alert("ao")}>{text}</button>
+          </div>
+        </div>
+    )
+  }
+  */
 
   return (
     <div className='root'>
@@ -272,17 +339,19 @@ function App() {
         }}
       />
 
-      <h1>
-        Mapis
-      </h1>
-
       {!showMap &&
+        <div className='landingPage'>
+          <h1>
+            Mapis
+          </h1>
+
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '20vh' }}>
             <GoogleLogin
               onSuccess={handleLogin}
               onError={() => console.error('Login Failed')}
               useOneTap={false}
             />
+          </div>
         </div>
       }
 
@@ -301,8 +370,17 @@ function App() {
           
           {markers.map((marker: any, i: number) => {
             return (
-              <Marker key={i} position={{lat: marker.position.lat, lng: marker.position.lng}}>
-                <Popup>{marker.description}</Popup>
+              <Marker
+                key={i}
+                position={{lat: marker.position.lat, lng: marker.position.lng}}
+                icon={marker.type === "piss" ? pissIcon : foodIcon}
+              >
+                <Popup>
+                  <div>
+                    {marker.description}
+                    <button onClick={() => getGoogleMapsDirections(marker.position.lat, marker.position.lng)}>Get directions</button>
+                  </div>
+                </Popup>
               </Marker>
             )
           })
@@ -317,7 +395,14 @@ function App() {
 
           {/* <ZoomOut /> */}
           <CurrentPosition />
-          <AddMarker />
+          <AddPissMarker />
+          <AddFoodMarker />
+          
+          {/*
+            <ButtonControl text={"Add piss marker"}/>
+            <ButtonControl text={"Add food marker"}/>
+          */}
+
           <MapPlaceholder />
         </MapContainer>
       }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import './index.css'
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId="798217786977-0egsto7cdcdv63l2vjqd226kdg572fq6.apps.googleusercontent.com">
